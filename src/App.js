d87e006124768918f9c61b6dd0c0f83aca7f1a57
DieheartACC <jaime.torres@austincc.edu>
2022-10-14 11:30:55 -0500

    Added post

--- src/App.js
+++ src/App.js
@@ -1,21 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
 import EmailList from './EmailList';
+import {useState} from "react";
 
 function App() {
+    const [recipientText, setRecipientText] = useState("");
+    const [subjectText, setSubjectText] = useState("");
+    const [messageText, setMessageText] = useState("");
+
+    const saveEmail = (e) => {
+        e.preventDefault();
+        fetch("http://localhost:3001/send", {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                sender: "jane@galvanize.con",
+                recipient: recipientText,
+                subject: subjectText,
+                message: messageText
+            })
+        })
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => console.log(data));
+    }
+
   return (
       <>
           <h1>Mock Gmail</h1>
           <div className="App">
               <EmailList/>
-              <form>
-                  <input type='email' id='sender' name='email' value='Senders email' onClick={() =>
-                      document.getElementById("sender").value = ""}/>
-                  <input type='text' id='subject' name='subject' value='subject' onClick={() =>
-                      document.getElementById("subject").value = ""}/>
-                  <input type='text' id='message' name='message' value='message' onClick={() =>
-                      document.getElementById("message").value = ""}/>
-                  <button type="submit" href='http://localhost:3001/emails/send'>Send Email</button>
+              <form onSubmit={(e) => saveEmail(e)}>
+                  <input type='email' id='recipient' name='email' placeholder='Recipient email' value={recipientText}
+                  onChange={(event) => {setRecipientText(event.target.value)}}/>
+                  <input type='text' id='subject' name='subject' placeholder='Subject'value={subjectText}
+                  onChange={(event) => {setSubjectText(event.target.value)}}/>
+                  <input type='text' id='message' name='message' placeholder='Message' value={messageText}
+                  onChange={(event) => {setMessageText(event.target.value)}}/>
+                  <button type="submit">Send Email</button>
               </form>
           </div>
       </>
@@ -23,4 +48,3 @@ function App() {
 }
 
 export default App;
-
